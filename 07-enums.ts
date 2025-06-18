9e673df08c04ce3158b02a94836b8de84431fa43
joaopedro-style <joaopedro18231@hotmail.com>
2025-06-18 11:10:25 -0300

    Cria enum

---
+++ 07-enums.ts
@@ -0,0 +1,14 @@
+// 07-enums.ts
+export {};
+
+/* Enums são uma maneira de definir um conjunto de constantes
+nomeadas. Úteis para definir: status, categorias, grupos de valores relacionados etc */
+enum DiasDaSemana {
+  Domingo = "Domingo",
+  Segunda = "Segunda-feira",
+  Terça = "Terça-feira",
+  Quarta = "Quarta-feira",
+}
+
+const diaAtual: DiasDaSemana = DiasDaSemana.Quarta;
+console.log(diaAtual);
